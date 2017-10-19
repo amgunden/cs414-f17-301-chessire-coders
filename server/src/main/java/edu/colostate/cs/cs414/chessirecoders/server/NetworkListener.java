9adf4fc360ee18c8f3725701b75c808af3a2607a
Cameron Ackerman <camja@rams.colostate.edu>
2017-10-19 12:38:46 -0600

    Added skeleton listener classes.

---
+++ server/src/main/java/edu/colostate/cs/cs414/chessirecoders/server/NetworkListener.java
@@ -0,0 +1,121 @@
+package edu.colostate.cs.cs414.chessirecoders.server;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Server;
+
+public class NetworkListener {
+
+    Server server;
+
+    public NetworkListener(Server server) {
+        this.server = server;
+        this.addListeners();
+    }
+
+    /**
+     *
+     */
+    private void addListeners() {
+
+        // LoginRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.LoginRequest>(ClientRequest.LoginRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.LoginRequest received) {
+
+            }
+        });
+
+        // LogoutRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.LogoutRequest>(ClientRequest.LogoutRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.LogoutRequest received) {
+
+            }
+        });
+
+        // RegisterRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.RegisterRequest>(ClientRequest.RegisterRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.RegisterRequest received) {
+
+            }
+        });
+
+        // GetGameRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.GetGameRequest>(ClientRequest.GetGameRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.GetGameRequest received) {
+
+            }
+        });
+
+        // GetPieceLocationRequestListener
+        server.addListener(new FilteredListener<ClientRequest.GetPieceLocationRequest>(ClientRequest.GetPieceLocationRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.GetPieceLocationRequest received) {
+
+            }
+        });
+
+        // UpdatePieceLocationRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.UpdatePieceLocationRequest>(ClientRequest.UpdatePieceLocationRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.UpdatePieceLocationRequest received) {
+
+            }
+        });
+
+        // GetPlayerRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.GetPlayerRequest>(ClientRequest.GetPlayerRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.GetPlayerRequest received) {
+
+            }
+        });
+
+        // GetUserRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.GetUserRequest>(ClientRequest.GetUserRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.GetUserRequest received) {
+
+            }
+        });
+
+        // UpdateInvitationRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.UpdateInvitationRequest>(ClientRequest.UpdateInvitationRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.UpdateInvitationRequest received) {
+
+            }
+        });
+
+        // UpdateSessionExpirationRequest Listener
+        server.addListener(new FilteredListener<ClientRequest.UpdateSessionExpirationRequest>(ClientRequest.UpdateSessionExpirationRequest.class) {
+            @Override
+            public void run(Connection connection, ClientRequest.UpdateSessionExpirationRequest received) {
+
+            }
+        });
+    }
+
+    /**
+     * @param <T>
+     */
+    public static abstract class FilteredListener<T> extends com.esotericsoftware.kryonet.Listener {
+
+        private final Class<T> c;
+
+        public FilteredListener(Class<T> c) {
+            this.c = c;
+        }
+
+        @Override
+        public void received(Connection connection, Object object) {
+            if (c.isInstance(object)) {
+                run(connection, c.cast(object));
+            }
+        }
+
+        public abstract void run(Connection connection, T received);
+    }
+}
