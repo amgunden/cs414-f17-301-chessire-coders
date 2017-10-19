f7738653b5b5f5f8806e842307f286016f97511b
ALHennings <AlexanderLeeHennings@gmail.com>
2017-10-19 02:06:54 -0600

    Adding GamePiece class and enum

---
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/Game/GamePiece.java
@@ -0,0 +1,31 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleClient.app.Game;
+
+public class GamePiece {
+	PieceType typeOfPiece = PieceType.Rat;
+	int row = 0;
+	int column = 0;
+	
+	public GamePiece(PieceType typeOfPiece, int row, int column) {
+		this.typeOfPiece = typeOfPiece;
+		this.row = row;
+		this.column = column;
+	}
+	
+	public PieceType getPieceType() {
+		return typeOfPiece;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+	//Possible Functions:
+	
+	public boolean canCapture(GamePiece comparisonPiece) {
+		return true;
+	}
+}
---
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/Game/PieceType.java
@@ -0,0 +1,5 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleClient.app.Game;
+
+public enum PieceType {
+Rat, Cat, Dog, Wolf, Leopard, Tiger, Lion, Elephant
+}
