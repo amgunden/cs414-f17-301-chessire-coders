378940ef29511cd2ba24759afeafb5bc9d617f4b
ALHennings <AlexanderLeeHennings@gmail.com>
2017-10-19 01:34:10 -0600

    Testing Implementation of Junit
    Aded JungleGame and TestJungleGame working on Junit

---
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/JungleGame.java
@@ -0,0 +1,16 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleClient.app;
+
+public class JungleGame {
+
+	public JungleGame() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
---
+++ client/src/test/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/util/TestJungleGame.java
@@ -0,0 +1,11 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleClient.util;
+import org.junit.*;
+
+public class TestJungleGame {
+
+	@Test
+	public void sayHi() {
+		System.out.println("hi");
+	}
+
+}
