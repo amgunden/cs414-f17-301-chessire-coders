5d31bad40b9150cfc18a123ab85e19012e3b3e49
tking2096 <kingtn@rams.colostate.edu>
2017-10-19 13:13:03 -0600

    Created GameBoardController.

---
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/GameBoardController.java
@@ -0,0 +1,15 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleClient.app;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class GameBoardController implements Initializable {
+
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
