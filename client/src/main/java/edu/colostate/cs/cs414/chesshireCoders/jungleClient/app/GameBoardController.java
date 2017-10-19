e581eb7ef0dfd70781fe25ca4e328608b43e179a
tking2096 <kingtn@rams.colostate.edu>
2017-10-19 15:31:54 -0600

    Made each square clickable.

--- client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/GameBoardController.java
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/GameBoardController.java
@@ -3,13 +3,31 @@ package edu.colostate.cs.cs414.chesshireCoders.jungleClient.app;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
 public class GameBoardController implements Initializable {
 
+	@FXML
+	public GridPane gridPane;
+	
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@FXML
+	public void squareClicked(MouseEvent event)
+	{
+		StackPane square = (StackPane) event.getSource();
+		int c = gridPane.getColumnIndex(square);
+		int r = gridPane.getRowIndex(square);
+
+		System.out.println("Square ("+r+","+c+") Clicked.");
 	}
 
 }
