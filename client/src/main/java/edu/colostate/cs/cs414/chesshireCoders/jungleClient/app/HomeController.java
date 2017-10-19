6cfed8077f682d908efd27540eef9af6177ed7ed
tking2096 <kingtn@rams.colostate.edu>
2017-10-19 11:10:22 -0600

    Updated home page ui.

--- client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/HomeController.java
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/HomeController.java
@@ -1,31 +1,49 @@
 package edu.colostate.cs.cs414.chesshireCoders.jungleClient.app;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
 public class HomeController implements Initializable {
 
 	@FXML
-	private ImageView btnPlay;
+	private Button btnPlay;
 	
 	@FXML
-	private ImageView btnViewProfile;
+	private Button btnLogout;
+	
+	@FXML
+	private ImageView btnSettings;
 	
 	@FXML
 	private ImageView btnViewInvites;
+	
+	@FXML
+	private Button btnViewGameHistory;
 	
 	public void playClicked()
 	{
 		System.out.println("Play Clicked.");
 	}
 	
-	public void viewProfileClicked()
+	public void viewGameHistoryClicked()
 	{
-		System.out.println("View Profile Clicked.");
+		System.out.println("View Game History Clicked.");
+	}
+	
+	public void logoutClicked()
+	{
+		System.out.println("Log Out Clicked.");
+		try {
+			App.setScene("loginPage.fxml");
+		} catch (IOException e) {
+			System.err.println("ERROR: Unable to load fxml file for Login page.");
+		}
 	}
 	
 	public void viewInvitesClicked()
@@ -33,9 +51,14 @@ public class HomeController implements Initializable {
 		System.out.println("View Invites Clicked.");
 	}
 	
+	public void settingsClicked()
+	{
+		System.out.println("Settings Clicked.");
+	}
+	
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-
+		App.window.setResizable(false);
 	}
 
 }
