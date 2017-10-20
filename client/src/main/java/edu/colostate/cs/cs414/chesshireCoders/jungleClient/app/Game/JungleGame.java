43ef4a2ddbfa4ed768b250b7b3503e6116b29edf
ALHennings <AlexanderLeeHennings@gmail.com>
2017-10-20 03:51:41 -0600

    Updating the JungleGame
    Otherwise this will be left alone until I have a chance to figure it out amongst the team. Currently this is the interaction point between the "game" and both the ui and server.

--- client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/Game/JungleGame.java
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/Game/JungleGame.java
@@ -3,39 +3,18 @@ package edu.colostate.cs.cs414.chesshireCoders.jungleClient.app.Game;
 import java.util.Date;
 
 public class JungleGame {
+	//NEED to find a datatype that will contain all the games if there are multiple. 
 	int GameID = 0;
 	Date startDateTime = new Date();
 	Date endDateTime;
 	int Status = 0;
 	//Status could be a String or an int with value of three (0,1,2) or (1,2,3) signifying each case. 
 	
+	//This entire class is IN DEVELOPMENT. I need to figure out it's exact purpose and how we want it to handle the game vs games. How it will be passing and accepting Data. And more. 
+	
+	
 	public JungleGame(int userIdOne, int userIdTwo) {
 		
-	}
-
-	public static void main(String[] args) {
-	}
-	
-	/*
-	public void move() {
-		//The Most important/common method. 
-		//This function has been broken down into the ones above.
-	}
-	*/
-	
-	public boolean checkMove() {
-		//This function should check the ability of the current peice to move in the requested way. Returning true iff it is completely possible. 
-		return true;
-	}
-	
-	public boolean commitMove() {
-		//This function should submit the move to the system, it should be called post checkMove but for now check Move is called preemptively. 
-		//Returns true iff the move is valid and the database accepts it. 
-		if(!checkMove()) {
-			return false;
-		}
-		
-		return true;
 	}
 	
 	public void sendInvitation() {
