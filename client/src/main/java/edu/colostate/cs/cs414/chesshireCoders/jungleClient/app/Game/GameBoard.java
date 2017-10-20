3d7526fa689ad91f7f27bd566c00c80fd45480f7
ALHennings <AlexanderLeeHennings@gmail.com>
2017-10-20 03:34:59 -0600

    Update Pieces to include color

--- client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/Game/GameBoard.java
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/Game/GameBoard.java
@@ -15,26 +15,44 @@ public class GameBoard {
 	public GameBoard(GamePiece[] gamePieces) {
 		this.gamePieces = gamePieces;
 	}
-
+	
+	public boolean movePiece(PieceType type, PlayerColor color, int row, int column) {
+		if(!checkMovement(type, color, row, column)) {
+			return false;
+		}
+		for(int i = 0; i < gamePieces.length; i++) {
+			if(gamePieces[i].getPieceType().equals(type)) {
+				if(gamePieces[i].getColor())
+			}
+		}
+		return false;
+	}
+	
+	public boolean checkMovement(PieceType type, PlayerColor color, int row, int column) {
+		return false;		
+	}
+	
+	//Board Setup Functions
+	
 	private void setUpPieces() {
 		//Player 1
-		gamePieces[0] = new GamePiece(PieceType.Rat, 0, 2);
-		gamePieces[1] = new GamePiece(PieceType.Cat, 5, 1);
-		gamePieces[2] = new GamePiece(PieceType.Dog, 1, 1);
-		gamePieces[3] = new GamePiece(PieceType.Wolf, 4, 2);
-		gamePieces[4] = new GamePiece(PieceType.Leopard, 2, 2);
-		gamePieces[5] = new GamePiece(PieceType.Tiger, 6, 0);
-		gamePieces[6] = new GamePiece(PieceType.Lion, 0, 0);
-		gamePieces[7] = new GamePiece(PieceType.Elephant, 6, 2);		
+		gamePieces[0] = new GamePiece(PieceType.Rat, 0, 2, PlayerColor.Red);
+		gamePieces[1] = new GamePiece(PieceType.Cat, 5, 1, PlayerColor.Red);
+		gamePieces[2] = new GamePiece(PieceType.Dog, 1, 1, PlayerColor.Red);
+		gamePieces[3] = new GamePiece(PieceType.Wolf, 4, 2, PlayerColor.Red);
+		gamePieces[4] = new GamePiece(PieceType.Leopard, 2, 2, PlayerColor.Red);
+		gamePieces[5] = new GamePiece(PieceType.Tiger, 6, 0, PlayerColor.Red);
+		gamePieces[6] = new GamePiece(PieceType.Lion, 0, 0, PlayerColor.Red);
+		gamePieces[7] = new GamePiece(PieceType.Elephant, 6, 2, PlayerColor.Red);		
 		//Player 2
-		gamePieces[8] = new GamePiece(PieceType.Rat, 6, 6);
-		gamePieces[9] = new GamePiece(PieceType.Cat, 1, 7);
-		gamePieces[10] = new GamePiece(PieceType.Dog, 5, 7);
-		gamePieces[11] = new GamePiece(PieceType.Wolf, 2, 6);
-		gamePieces[12] = new GamePiece(PieceType.Leopard, 4, 6);
-		gamePieces[13] = new GamePiece(PieceType.Tiger, 0, 8);
-		gamePieces[14] = new GamePiece(PieceType.Lion, 6, 8);
-		gamePieces[15] = new GamePiece(PieceType.Elephant, 0, 6);
+		gamePieces[8] = new GamePiece(PieceType.Rat, 6, 6, PlayerColor.Black);
+		gamePieces[9] = new GamePiece(PieceType.Cat, 1, 7, PlayerColor.Black);
+		gamePieces[10] = new GamePiece(PieceType.Dog, 5, 7, PlayerColor.Black);
+		gamePieces[11] = new GamePiece(PieceType.Wolf, 2, 6, PlayerColor.Black);
+		gamePieces[12] = new GamePiece(PieceType.Leopard, 4, 6, PlayerColor.Black);
+		gamePieces[13] = new GamePiece(PieceType.Tiger, 0, 8, PlayerColor.Black);
+		gamePieces[14] = new GamePiece(PieceType.Lion, 6, 8, PlayerColor.Black);
+		gamePieces[15] = new GamePiece(PieceType.Elephant, 0, 6, PlayerColor.Black);
 	}
 	
 	private void setUpBoard() {
--- client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/Game/GamePiece.java
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/Game/GamePiece.java
@@ -4,11 +4,23 @@ public class GamePiece {
 	PieceType typeOfPiece = PieceType.Rat;
 	int row = 0;
 	int column = 0;
+	PlayerColor color;
 	
-	public GamePiece(PieceType typeOfPiece, int row, int column) {
+	public GamePiece(PieceType typeOfPiece, int row, int column, PlayerColor color) {
 		this.typeOfPiece = typeOfPiece;
 		this.row = row;
 		this.column = column;
+		this.color = color;
+	}
+	
+	public void changeLocation(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	//Getter/Setters
+	public PlayerColor getColor() {
+		return color;
 	}
 	
 	public PieceType getPieceType() {
