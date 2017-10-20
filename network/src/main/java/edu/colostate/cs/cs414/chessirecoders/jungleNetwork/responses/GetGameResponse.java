95b08fd27883137e7962521a930ad368fe8c1b8f
Cameron Ackerman <camja@rams.colostate.edu>
2017-10-19 19:04:21 -0600

    Fixed build errors.

--- network/src/main/java/edu/colostate/cs/cs414/chessirecoders/jungleNetwork/responses/GetGameResponse.java
+++ network/src/main/java/edu/colostate/cs/cs414/chessirecoders/jungleNetwork/responses/GetGameResponse.java
@@ -1,15 +1,17 @@
 package edu.colostate.cs.cs414.chessirecoders.jungleNetwork.responses;
 
-import edu.colostate.cs.cs414.chessirecoders.JungleServer.data.Types;
+
+import edu.colostate.cs.cs414.chessirecoders.jungleNetwork.types.GameStatus;
+import edu.colostate.cs.cs414.chessirecoders.jungleNetwork.types.PlayerStatus;
 
 public class GetGameResponse {
     int gameID;
     int playerOneID;
     int playerTwoID;
-    Types.PlayerStatus playerTwoStatus;
-    Types.GameStatus status;
+    PlayerStatus playerTwoStatus;
+    GameStatus status;
 
-    public GetGameResponse(int gameID, int playerOneID, int playerTwoID, Types.PlayerStatus playerTwoStatus, Types.GameStatus status) {
+    public GetGameResponse(int gameID, int playerOneID, int playerTwoID, PlayerStatus playerTwoStatus, GameStatus status) {
         this.gameID = gameID;
         this.playerOneID = playerOneID;
         this.playerTwoID = playerTwoID;
--- network/src/main/java/edu/colostate/cs/cs414/chessirecoders/jungleNetwork/responses/GetPlayerResponse.java
+++ network/src/main/java/edu/colostate/cs/cs414/chessirecoders/jungleNetwork/responses/GetPlayerResponse.java
@@ -1,13 +1,14 @@
 package edu.colostate.cs.cs414.chessirecoders.jungleNetwork.responses;
 
-import edu.colostate.cs.cs414.chessirecoders.JungleServer.data.Types;
+
+import edu.colostate.cs.cs414.chessirecoders.jungleNetwork.types.PlayerColor;
 
 public class GetPlayerResponse {
     int playerID;
     String playerName;
-    Types.PlayerColor playerColor;
+    PlayerColor playerColor;
 
-    public GetPlayerResponse(int playerID, String playerName, Types.PlayerColor playerColor) {
+    public GetPlayerResponse(int playerID, String playerName, PlayerColor playerColor) {
         this.playerID = playerID;
         this.playerName = playerName;
         this.playerColor = playerColor;
