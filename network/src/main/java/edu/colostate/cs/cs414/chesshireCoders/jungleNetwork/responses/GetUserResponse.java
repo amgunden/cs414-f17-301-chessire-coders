a6f93ee152f36c076d2d39284d06d905ffb76e56
amgunden <amgunden@gmail.com>
2017-10-24 20:44:33 -0600

    Added handlers/responses for GetPlayer and GetUser

---
+++ network/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleNetwork/responses/GetUserResponse.java
@@ -0,0 +1,52 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.responses;
+
+public class GetUserResponse extends Response{
+
+	private int UserID;
+	private String firstName;
+	private String lastName;
+	private String nickName;
+	
+	public GetUserResponse() {
+//		this.UserID = userID;
+//		this.firstName = firstName;
+//		this.lastName = lastName;
+//		this.nickName = nickName;
+	}
+	
+    public GetUserResponse(int statusCode, String message) {
+        super(statusCode, message);
+    }
+	
+	public int getUserID() {
+		return UserID;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getNickName() {
+		return nickName;
+	}
+	
+	public void setUserID(int userid) {
+		this.UserID = userid;
+	}
+	
+	public void setFirstName(String firstname) {
+		this.firstName= firstname;
+	}
+	
+	public void setLastName(String lastname) {
+		this.lastName = lastname;
+	}
+	
+	public void setNickName(String nickname) {
+		this.nickName =  nickname;
+	}
+}
---
+++ server/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleServer/handlers/GetUserHandler.java
@@ -0,0 +1,58 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleServer.handlers;
+
+import static edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.responses.ResponseStatusCodes.SERVER_ERROR;
+
+import java.sql.SQLException;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+
+import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.listeners.FilteredListener;
+import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.requests.GetUserRequest;
+import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.responses.GetUserResponse;
+import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.responses.Response;
+import edu.colostate.cs.cs414.chesshireCoders.jungleServer.dataAccessObjects.UserDAO;
+import edu.colostate.cs.cs414.chesshireCoders.jungleServer.dataObjects.User;
+import edu.colostate.cs.cs414.chesshireCoders.jungleServer.server.JungleServer;
+
+public class GetUserHandler extends AbstractRequestHandler{
+
+	public GetUserHandler(JungleServer server) {
+		super(server);
+		// TODO Auto-generated constructor stub
+	}
+
+    @Override
+    public void addListeners() {
+        // GetUserRequest Handler
+        server.addListener(new Listener.ThreadedListener(
+                new FilteredListener<GetUserRequest>(GetUserRequest.class) {
+                    @Override
+                    public void run(Connection connection, GetUserRequest received) {
+                        connection.sendTCP(handleGetUser(received));
+                    }
+                }));
+    }
+    
+    private Response handleGetUser(GetUserRequest request) {
+    	UserDAO userDAO = new UserDAO();
+    	
+    	try {
+            try {
+            	userDAO.getConnection();
+            	User user = userDAO.getUserByUserId(request.getUserID());
+            	GetUserResponse response = new GetUserResponse();
+            	response.setUserID(user.getUserId());
+            	response.setFirstName(user.getNameFirst());
+            	response.setLastName(user.getNameLast());
+            	response.setNickName(user.getNickName());
+                return response;
+
+            } finally {
+                userDAO.closeConnection();
+            }
+        } catch (SQLException e) {
+            return new GetUserResponse(SERVER_ERROR, e.getMessage());
+        }  	
+    }
+
+}
