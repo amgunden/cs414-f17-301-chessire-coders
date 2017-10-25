ca7cf5045d171c80a947e60c268505cde9014168
Cameron Ackerman <camja@rams.colostate.edu>
2017-10-24 21:49:50 -0600

    Added handling for LoginRequest.

--- server/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleServer/handlers/SessionHandler.java
+++ server/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleServer/handlers/SessionHandler.java
@@ -1,15 +1,22 @@
 package edu.colostate.cs.cs414.chesshireCoders.jungleServer.handlers;
 
 import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.EndPoint;
 import com.esotericsoftware.kryonet.Listener;
 import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.listeners.FilteredListener;
 import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.requests.LoginRequest;
 import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.requests.LogoutRequest;
 import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.requests.UpdateSessionExpirationRequest;
+import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.responses.LoginResponse;
+import edu.colostate.cs.cs414.chesshireCoders.jungleNetwork.responses.ResponseStatusCodes;
 import edu.colostate.cs.cs414.chesshireCoders.jungleServer.server.JungleServer;
+import edu.colostate.cs.cs414.chesshireCoders.jungleServer.session.LoginManager;
+import edu.colostate.cs.cs414.chesshireCoders.jungleServer.session.LoginVerifier;
+
+import java.sql.SQLException;
 
 public class SessionHandler extends AbstractRequestHandler {
+
+    LoginManager manager = new LoginManager();
 
     public SessionHandler(JungleServer server) {
         super(server);
@@ -22,8 +29,11 @@ public class SessionHandler extends AbstractRequestHandler {
                 new FilteredListener<LoginRequest>(LoginRequest.class) {
                     @Override
                     public void run(Connection connection, LoginRequest received) {
-                        // TODO: delegate to login verifier
-
+                        try {
+                            connection.sendTCP(handleLoginRequest(connection, received));
+                        } catch (SQLException e) {
+                            connection.sendTCP(new LoginResponse(ResponseStatusCodes.SERVER_ERROR, e.getMessage()));
+                        }
                     }
                 }));
 
@@ -46,5 +56,30 @@ public class SessionHandler extends AbstractRequestHandler {
                 }));
     }
 
+    private LoginResponse handleLoginRequest(Connection connection, LoginRequest request) throws SQLException {
+        LoginVerifier verifier = new LoginVerifier(request.getEmail(), request.getPassword());
+
+        // Check if registered
+        if (verifier.isEmailRegistered()) {
+            // Check if password okay
+            if (verifier.isPasswordOkay()) {
+                // Log user in
+                manager.loginUser(connection, verifier.getUserInfo());
+                return new LoginResponse(
+                        ResponseStatusCodes.SUCCESS,
+                        "Login successful."); // TODO: set expiration, and session token.
+            } else {
+                // Handle bad password
+                return new LoginResponse(
+                        ResponseStatusCodes.CLIENT_ERROR,
+                        "Bad Password.");
+            }
+        } else {
+            // handle bad email
+            return new LoginResponse(
+                    ResponseStatusCodes.CLIENT_ERROR,
+                    "Email is not registered");
+        }
+    }
 
 }
