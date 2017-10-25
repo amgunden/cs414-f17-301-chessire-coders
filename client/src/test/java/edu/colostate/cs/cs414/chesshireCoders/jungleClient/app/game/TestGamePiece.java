32641d12666c7c6c4ff76e1bb24e610f59f4cb46
tking2096 <kingtn@rams.colostate.edu>
2017-10-24 20:45:48 -0600

    Created test class.

---
+++ client/src/test/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/app/game/TestGamePiece.java
@@ -0,0 +1,24 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleClient.app.game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGamePiece {
+
+	@Test
+	public void testChangeLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPowerLevel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanOccupy() {
+		fail("Not yet implemented");
+	}
+
+}
