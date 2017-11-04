d359dd2ec6ccb96a8206e75725572e2318d3c07b
Cameron Ackerman <ackerman.cameron@gmail.com>
2017-11-04 17:47:12 -0600

    Added class for password hashing and token generation.

---
+++ util/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleUtil/security/Crypto.java
@@ -0,0 +1,24 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleUtil.security;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public final class Crypto {
+
+    public static final int AUTH_TOKEN_LENGTH = 16;
+
+    public static String generateAuthToken() {
+        SecureRandom random = new SecureRandom();
+        byte[] token = new byte[AUTH_TOKEN_LENGTH];
+        random.nextBytes(token);
+        return Base64.getEncoder().encodeToString(token);
+    }
+
+    public static String hashSHA256(byte[] bytes) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA256");
+        byte[] digestedBytes = digest.digest(bytes);
+        return Base64.getEncoder().encodeToString(digestedBytes);
+    }
+}
