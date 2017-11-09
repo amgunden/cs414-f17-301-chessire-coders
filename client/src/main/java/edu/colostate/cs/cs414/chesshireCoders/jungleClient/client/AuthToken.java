59516cc3af7f16d4d3708420e7fe0bbd989f3d04
Cameron Ackerman <ackerman.cameron@gmail.com>
2017-11-09 09:51:38 -0700

    Added AuthToken class.

---
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/client/AuthToken.java
@@ -0,0 +1,35 @@
+package edu.colostate.cs.cs414.chesshireCoders.jungleClient.client;
+
+public class AuthToken {
+
+    private static AuthToken token;
+
+    private String authenticationToken;
+
+    private AuthToken() {
+    }
+
+    public static AuthToken getInstance() {
+        if (token == null) {
+            synchronized (AuthToken.class) {
+                token = new AuthToken();
+            }
+        }
+
+        return token;
+    }
+
+    public static void load(String file) {
+    }
+
+    public static void save(String file) {
+    }
+
+    public String getToken() {
+        return authenticationToken;
+    }
+
+    public void setToken(String token) {
+        this.authenticationToken = authenticationToken;
+    }
+}
