eecbe90bf8a698574cebce539756c7031a060328
Cameron Ackerman <camja@rams.colostate.edu>
2017-11-12 15:18:00 -0700

    Added invitation table definition.

--- docker/postgres/docker-entrypoint-initdb.d/init-tables-db.sql
+++ docker/postgres/docker-entrypoint-initdb.d/init-tables-db.sql
@@ -94,12 +94,35 @@ CREATE TABLE game_piece
   piece_type      PIECETYPE,
   position_row    INTEGER     NOT NULL,
   CONSTRAINT chk_game_piece_row
-    CHECK (0 <= position_row AND position_row < 9),
+  CHECK (0 <= position_row AND position_row < 9),
   position_column INTEGER     NOT NULL
-  CONSTRAINT chk_game_piece_column
+    CONSTRAINT chk_game_piece_column
     CHECK (0 <= position_column AND position_column < 7)
 );
 
 ALTER TABLE game_piece
   ADD CONSTRAINT unq_row_column_game
 UNIQUE (position_row, position_column, game_id);
+
+CREATE TYPE INVITESTATUS AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
+CREATE TABLE invitation
+(
+  invitation_id     BIGSERIAL                      NOT NULL
+    CONSTRAINT invitation_pkey
+    PRIMARY KEY,
+  user_sender_id    BIGINT                         NOT NULL
+    CONSTRAINT fk_invitation_sender_id
+    REFERENCES jungle_user,
+  user_recipient_id BIGINT                         NOT NULL
+    CONSTRAINT fk_invitation_recipient_id
+    REFERENCES jungle_user,
+  game_id           BIGINT                         NOT NULL
+    CONSTRAINT fk_invitation_game_id
+    REFERENCES game,
+  invite_status     INVITESTATUS DEFAULT 'PENDING' NOT NULL
+);
+
+ALTER TABLE invitation
+  ADD CONSTRAINT unq_invitation_sender_recipient_game
+UNIQUE (user_sender_id, user_recipient_id, game_id);
