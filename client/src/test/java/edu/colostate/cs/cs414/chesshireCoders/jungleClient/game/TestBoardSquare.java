2466b07b9f0199b1974b0ff1b8677b15d8fa9085
Cameron Ackerman <camja@rams.colostate.edu>
2017-11-30 20:09:15 -0700

    organized imports.

--- client/src/test/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/game/TestBoardSquare.java
+++ client/src/test/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/game/TestBoardSquare.java
@@ -1,11 +1,7 @@
 package edu.colostate.cs.cs414.chesshireCoders.jungleClient.game;
 
-import org.junit.Test;
-
-import edu.colostate.cs.cs414.chesshireCoders.jungleClient.game.BoardSquare;
-import edu.colostate.cs.cs414.chesshireCoders.jungleClient.game.JungleGamePiece;
-import edu.colostate.cs.cs414.chesshireCoders.jungleClient.game.RatPiece;
 import edu.colostate.cs.cs414.chesshireCoders.jungleUtil.types.PlayerEnumType;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
