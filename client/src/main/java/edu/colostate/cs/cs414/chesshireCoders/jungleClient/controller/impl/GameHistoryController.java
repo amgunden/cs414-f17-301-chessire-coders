2008698ee22423a2d1b4dbd5445beecdf7853fbb
amgunden <amgunden@gmail.com>
2017-12-08 09:38:00 -0700

    Changed from ListView to TableView

--- client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/controller/impl/GameHistoryController.java
+++ client/src/main/java/edu/colostate/cs/cs414/chesshireCoders/jungleClient/controller/impl/GameHistoryController.java
@@ -1,15 +1,25 @@
 package edu.colostate.cs.cs414.chesshireCoders.jungleClient.controller.impl;
 
+import java.text.DateFormat;
+
 import edu.colostate.cs.cs414.chesshireCoders.jungleClient.controller.BaseController;
+import edu.colostate.cs.cs414.chesshireCoders.jungleClient.model.AccountModel;
 import edu.colostate.cs.cs414.chesshireCoders.jungleClient.model.GameHistoryModel;
 import edu.colostate.cs.cs414.chesshireCoders.jungleClient.view.View;
-import edu.colostate.cs.cs414.chesshireCoders.jungleClient.view.impl.ui.GameListCell;
 import edu.colostate.cs.cs414.chesshireCoders.jungleClient.view.impl.ui.InviteListCell;
 import edu.colostate.cs.cs414.chesshireCoders.jungleUtil.game.Game;
+import edu.colostate.cs.cs414.chesshireCoders.jungleUtil.types.GameStatus;
 import javafx.beans.Observable;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
 
 
 
@@ -24,7 +34,15 @@ public class GameHistoryController{
 	@FXML
 	private Label drawsNum;
 	@FXML 
-	private ListView<Game> historyList;
+	private TableView<Game> historyTable;
+	@FXML
+	private TableColumn opponent;
+	@FXML
+	private TableColumn startTime;
+	@FXML
+	private TableColumn endTime;
+	@FXML
+	private TableColumn result;
 	
 	GameHistoryModel history;
 	
@@ -35,15 +53,110 @@ public class GameHistoryController{
 	
 	public void setLabels() {
 		System.out.println(history.getNickname());
+		
 		profileNickName.setText(history.getNickname());
 		winsNum.setText(Integer.toString(history.getWins()));
 		lossesNum.setText(Integer.toString(history.getLosses()));
 		drawsNum.setText(Integer.toString(history.getDraws()));
 		
-		historyList.setCellFactory(param -> new GameListCell());
-		historyList.setItems(history.getGames());
+		opponent.setCellValueFactory(new opponentTableCell() );
+		startTime.setCellValueFactory(new startTimeTableCell() );
+		endTime.setCellValueFactory(new endTimeTableCell() );
+		result.setCellValueFactory(new resultTableCell() );
+			
+		historyTable.setItems(history.getGames());
+		historyTable.getColumns().setAll(opponent, startTime, endTime, result);
+
+		
+		
 		//historyList.getItems().addAll(history.getGames());
 
 	}
 
+}
+
+class opponentTableCell implements Callback<TableColumn.CellDataFeatures<Game, String>, ObservableValue<String>>{
+	
+	AccountModel accountModel = AccountModel.getInstance();
+	@Override
+	public ObservableValue<String> call(CellDataFeatures<Game, String> game) {
+		
+    	if (game.getValue().getGameStatus() == GameStatus.DRAW) {
+			// Winner & Opponent is Player Two
+			if (game.getValue().getPlayerOneNickName().equals(accountModel.getNickName())) return new ReadOnlyStringWrapper( game.getValue().getPlayerTwoNickName());
+			// Loser & Opponent is Player One
+			else return new ReadOnlyStringWrapper(game.getValue().getPlayerTwoNickName());
+    		
+		} else if (game.getValue().getGameStatus() == GameStatus.WINNER_PLAYER_ONE) {
+			// Winner & Opponent is Player Two
+			if (game.getValue().getPlayerOneNickName().equals(accountModel.getNickName())) return new ReadOnlyStringWrapper( game.getValue().getPlayerTwoNickName());
+			// Loser & Opponent is Player One
+			else return new ReadOnlyStringWrapper( game.getValue().getPlayerTwoNickName());
+
+		} else if (game.getValue().getGameStatus() == GameStatus.WINNER_PLAYER_TWO) {
+			// Winner & Opponent is Player One
+			if (game.getValue().getPlayerTwoNickName().equals(accountModel.getNickName())) return new ReadOnlyStringWrapper( game.getValue().getPlayerOneNickName());
+			// Loser & Opponent is Player Two
+			else return new ReadOnlyStringWrapper( game.getValue().getPlayerTwoNickName());
+		}
+    	return new ReadOnlyStringWrapper("Error Retrieveing Opponent");
+	}
+	
+}
+
+class startTimeTableCell implements Callback<TableColumn.CellDataFeatures<Game, String>, ObservableValue<String>>{
+	
+	AccountModel accountModel = AccountModel.getInstance();
+	@Override
+	public ObservableValue<String> call(CellDataFeatures<Game, String> game) {
+		
+    	if( game.getValue().getGameStart() != null) {
+    		return new ReadOnlyStringWrapper( DateFormat.getInstance().format(game.getValue().getGameStart()));
+    	}
+    	else {
+    		return new ReadOnlyStringWrapper("Unknown");
+    	}
+	}
+	
+}
+
+class endTimeTableCell implements Callback<TableColumn.CellDataFeatures<Game, String>, ObservableValue<String>>{
+	
+	AccountModel accountModel = AccountModel.getInstance();
+	@Override
+	public ObservableValue<String> call(CellDataFeatures<Game, String> game) {
+		
+    	if( game.getValue().getGameEnd() != null) {
+    		return new ReadOnlyStringWrapper( DateFormat.getInstance().format(game.getValue().getGameEnd()));
+    	}
+    	else {
+    		return new ReadOnlyStringWrapper("Unknown");
+    	}
+	}
+	
+}
+
+class resultTableCell implements Callback<TableColumn.CellDataFeatures<Game, String>, ObservableValue<String>>{
+	
+	AccountModel accountModel = AccountModel.getInstance();
+	@Override
+	public ObservableValue<String> call(CellDataFeatures<Game, String> game) {
+		
+		if (game.getValue().getGameStatus() == GameStatus.DRAW) {
+    		return new ReadOnlyStringWrapper("Draw");    		
+		} else if (game.getValue().getGameStatus() == GameStatus.WINNER_PLAYER_ONE) {
+			// Winner & Opponent is Player Two
+			if (game.getValue().getPlayerOneNickName().equals(accountModel.getNickName())) return new ReadOnlyStringWrapper("Win");
+			// Loser & Opponent is Player One
+			else return new ReadOnlyStringWrapper("Loss");
+			
+		} else if (game.getValue().getGameStatus() == GameStatus.WINNER_PLAYER_TWO) {
+			// Winner & Opponent is Player One
+			if (game.getValue().getPlayerTwoNickName().equals(accountModel.getNickName())) return new ReadOnlyStringWrapper("Win");
+			// Loser & Opponent is Player Two
+			else return new ReadOnlyStringWrapper("Loss");
+		}
+		return new ReadOnlyStringWrapper("Error Retrieveing Result");
+	}
+	
 }
